8e46f3a34306493b54691f24b37072290684fb93
Laurant <shklaurant@gmail.com>
2022-11-15 16:16:50 +0900

    added transform_data

--- editor/src/data/transform_data.cpp
+++ editor/src/data/transform_data.cpp
@@ -1,1 +1,23 @@
 #include "transform_data.h"
+#include "helpers/yaml.h"
+
+void editor::transform_data::serialize(YAML::Emitter &em) const {
+    em << YAML::Key << "transform_component";
+    em << YAML::BeginMap;
+    em << YAML::Key << "translate" << YAML::Value << translate;
+    em << YAML::Key << "rotation" << YAML::Value << rotation;
+    em << YAML::Key << "scale" << YAML::Value << scale;
+    em << YAML::EndMap;
+}
+
+void editor::transform_data::try_deserialize(const YAML::Node &node) {
+    if(node["transform_component"]){
+        deserialize(node["transform_component"]);
+    }
+}
+
+void editor::transform_data::deserialize(const YAML::Node &node) {
+    translate = node["translate"].as<fuse::vec2f>();
+    rotation = node["rotation"].as<float>();
+    scale = node["scale"].as<fuse::vec2f>();
+}
--- editor/src/data/transform_data.h
+++ editor/src/data/transform_data.h
@@ -1,6 +1,16 @@
 #pragma once
 
+#include "entity_data.h"
+#include "math/vec2.h"
 
-class transform_data {
+namespace editor {
+    struct transform_data : public component_data {
+        void serialize(YAML::Emitter &emitter) const override;
+        void try_deserialize(const YAML::Node &node) override;
+        void deserialize(const YAML::Node &node) override;
 
-};
+        fuse::vec2f translate;
+        float rotation;
+        fuse::vec2f scale;
+    };
+}
