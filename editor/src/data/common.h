0dae7cc5ce16040fe2d77825687b29ab25f4d6c5
Laurant <shklaurant@gmail.com>
2022-11-16 11:17:53 +0900

    minor clean up

--- editor/src/data/common.h
+++ editor/src/data/common.h
@@ -15,12 +15,12 @@ namespace editor {
     };
 
     struct transform_data : public component_data {
-        void serialize(YAML::Emitter &emitter) const override;
-        void try_deserialize(const YAML::Node &node) override;
-        void deserialize(const YAML::Node &node) override;
-
         fuse::vec2f translate;
         float rotation;
         fuse::vec2f scale;
+
+        void serialize(YAML::Emitter &emitter) const override;
+        void try_deserialize(const YAML::Node &node) override;
+        void deserialize(const YAML::Node &node) override;
     };
 }
